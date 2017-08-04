94481e7fe1ff36e6d6955ea36c9d7873a8b443cf
Patrick Kunka <patrick@kunkalabs.com>
2017-08-04 14:42:23 +0100

    Implement Router findMatchingRoute() functionality, tests passing

--- src/generic/Router.js
+++ src/generic/Router.js
@@ -23,19 +23,101 @@ class Router {
     }
 
     /**
-     * Tests a provided path against all routes, returning a popualed
+     * Tests a provided path against all routes, returning a populated
      * `Route` object if matching.
      *
-     * @param  {string} path
+     * @param  {string} pathRaw
+     * @param  {string} [queryString='']
      * @return {Route}
      */
 
-    findMatchingRoute(path) {
+    findMatchingRoute(pathRaw, queryString='') {
         if (this.routes.length < 1) throw new Error(ERROR_NOT_INITIALISED);
+
+        if (typeof pathRaw !== 'string') throw new TypeError(ERROR_INVALID_PATH);
+
+        const path = Router.sanitiseUrl(pathRaw);
+
+        let request = null;
+        let route   = null;
+        let i       = -1;
+
+        for (i = 0; i < this.captures.length; i++) {
+            const capture = this.captures[i];
+
+            request = capture.test(path);
+
+            if (request) break;
+        }
+
+        if (!request) {
+            throw new Error(ERROR_NOT_FOUND);
+        }
+
+        request.queryString = queryString;
+
+        Object.assign(request.query, Router.parseQueryString(queryString));
+
+        route = Object.assign(new Route(), this.routes[i]);
+
+        route.request = request;
+
+        Object.freeze(route);
+
+        return route;
+    }
+
+    /**
+     * Cleans up common typos in a provided URL path.
+     *
+     * @static
+     * @param  {string} pathRaw
+     * @return {string}
+     */
+
+    static sanitiseUrl(pathRaw) {
+        // - convert to toLowerCase()
+        // - strip whitespace
+        // - replace multiple back slashes with one slash
+        // - replace multiple forward slashes with one slash
+        // - replace back slashes with forward slashes
+        // - add leading slash
+        // - add trailing slash
+
+        return pathRaw
+            .toLowerCase()
+            .replace(/\s/g, '')
+            .replace(/\\\\+/g, '\\')
+            .replace(/\\/g, '/')
+            .replace(/\/\/+/g, '/')
+            .replace(/^\/?/, '/')
+            .replace(/\/?$/, '/');
+    }
+
+    /**
+     * Parses a serialised query string into a key/value hash.
+     *
+     * @static
+     * @param  {string} queryString
+     * @return {object}
+     */
+
+    static parseQueryString(queryStringRaw) {
+        const queryString = queryStringRaw.replace('?', '');
+
+        return queryString.split('&').reduce((query, parameter) => {
+            const [key, value] = parameter.split('=');
+
+            query[key] = typeof value === 'undefined' ? true : value;
+
+            return query;
+        }, {});
     }
 }
 
 export const ERROR_INVALID_ROUTES  = '[Router#init()] Invalid routes';
-export const ERROR_NOT_INITIALISED = '[Router#followPath()] Not initialised';
+export const ERROR_INVALID_PATH    = '[Router#findMatchingRoute()] Invalid path';
+export const ERROR_NOT_FOUND       = '[Router#findMatchingRoute()] No matching route found';
+export const ERROR_NOT_INITIALISED = '[Router#findMatchingRoute()] Not initialised';
 
 export default Router;
--- src/generic/Router.test.js
+++ src/generic/Router.test.js
@@ -1,6 +1,8 @@
 import Router, {
     ERROR_NOT_INITIALISED,
-    ERROR_INVALID_ROUTES
+    ERROR_INVALID_ROUTES,
+    ERROR_INVALID_PATH,
+    ERROR_NOT_FOUND
 } from './Router';
 
 import Capture from './Capture';
@@ -56,10 +58,7 @@ describe('Router', () => {
             const router = new Router();
 
             router.init([
-                {
-                    pattern: '/',
-                    action: null
-                }
+                {pattern: '/'}
             ]);
 
             assert.equal(router.captures.length, 1);
@@ -69,6 +68,84 @@ describe('Router', () => {
     });
 
     describe('#findMatchingRoute', () => {
+        it('should throw an error if an invalid path is passed', () => {
+            const router = new Router();
 
+            router.init([
+                {pattern: '/'}
+            ]);
+
+            assert.throws(() => router.findMatchingRoute(void(0)), ERROR_INVALID_PATH);
+        });
+
+        it('should throw an error if no matching route is found', () => {
+            const router = new Router();
+
+            router.init([
+                {pattern: '/'}
+            ]);
+
+            assert.throws(() => router.findMatchingRoute('/foo/'), ERROR_NOT_FOUND);
+        });
+
+        it('should return a populated instance of a `Route` for matching paths', () => {
+            const router = new Router();
+            const action = () => (void(0));
+
+            router.init([
+                {pattern: '/', action}
+            ]);
+
+            const route = router.findMatchingRoute('/');
+
+            assert.instanceOf(route, Route);
+            assert.equal(route.pattern, '/');
+            assert.equal(route.action, action);
+        });
+
+        it('should return a populated instance of a `Route` for matching paths with dynamic segments', () => {
+            const router = new Router();
+
+            router.init([
+                {pattern: '/:dynamicSegment/'}
+            ]);
+
+            const route = router.findMatchingRoute('/foo/');
+
+            assert.instanceOf(route, Route);
+            assert.equal(route.pattern, '/:dynamicSegment/');
+            assert.equal(route.request.path, '/foo/');
+            assert.equal(route.request.params.dynamicSegment, 'foo');
+        });
+
+        it('should parse an associated query string into a hash', () => {
+            const router = new Router();
+
+            router.init([
+                {pattern: '/:dynamicSegment/'}
+            ]);
+
+            const route = router.findMatchingRoute('/foo/', '?bar=baz');
+
+            assert.instanceOf(route, Route);
+            assert.equal(route.pattern, '/:dynamicSegment/');
+            assert.equal(route.request.path, '/foo/');
+            assert.equal(route.request.params.dynamicSegment, 'foo');
+            assert.equal(route.request.queryString, '?bar=baz');
+            assert.equal(route.request.query.bar, 'baz');
+        });
+
+        it('should sanitize the provided path', () => {
+            const router = new Router();
+
+            router.init([
+                {pattern: '/required-segment/'}
+            ]);
+
+            const route = router.findMatchingRoute(' /required-segment ');
+
+            assert.instanceOf(route, Route);
+            assert.equal(route.request.path, '/required-segment/');
+        });
     });
 });
