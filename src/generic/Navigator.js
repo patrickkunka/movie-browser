c782000a12596e6050b56c3423c725e135a5513b
Patrick Kunka <patrick@kunkalabs.com>
2017-08-04 16:40:32 +0100

    Implement Navigator class to handle navigation/traversal

--- src/generic/Navigator.js
+++ src/generic/Navigator.js
@@ -8,6 +8,41 @@ class Navigator {
 
         this.bindHandlers();
     }
+
+    /**
+     * @return {void}
+     */
+
+    bindHandlers() {
+        document.body.addEventListener('click', this.handleClick.bind(this));
+        window.addEventListener('popState', this.handlePopState.bind(this));
+    }
+
+    /**
+     * @param  {MouseEvent} e
+     * @return {void}
+     */
+
+    handleClick(e) {
+        const anchor = e.target.matches('a[href]') ? e.target : e.closest('a[href]');
+
+        let href = '';
+
+        if (!anchor || (href = anchor.href) === '') return;
+
+        this.stateManager.navigate(href)
+            .catch(err => console.error(err));
+    }
+
+    /**
+     * @param  {PopStateEvent} e
+     * @return {void}
+     */
+
+    handlePopState(e) {
+        this.stateManager.receivePoppedState(e.state)
+            .catch(err => console.error(err));
+    }
 }
 
 export const ERROR_STATE_MANAGER_NOT_INJECTED = '[Navigator] State manager must be injected';
