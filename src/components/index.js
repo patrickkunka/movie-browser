56e04b0d12f8386b198a09323debddaf41a28b0e
Patrick Kunka <patrick@kunkalabs.com>
2017-08-04 10:56:09 +0100

    model application view structure and scaffold UI components

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+// @create-index
+
+export { default as Movie } from './Movie.js';
+export { default as Root } from './Root.js';
+export { default as SearchForm } from './SearchForm.js';
+export { default as SearchResult } from './SearchResult.js';
+export { default as SearchResults } from './SearchResults.js';
+export { default as Suggestion } from './Suggestion.js';
+export { default as Suggestions } from './Suggestions.js';
+
---
+++ src/layouts/root.js
@@ -0,0 +1,47 @@
+import {
+    Root,
+    SearchForm,
+    Suggestions,
+    Suggestion,
+    SearchResults,
+    SearchResult,
+    Movie,
+    Loader
+} from './components';
+
+export default {
+    component: Root,
+    children: [
+        {
+            component: SearchForm,
+            if: state => state.isSearchView,
+            children: [
+                {
+                    component: Suggestions,
+                    if: state => state.hasSuggestions,
+                    children: [
+                        {
+                            component: Suggestion,
+                            forEach: state => state.suggestions
+                        }
+                    ]
+                }
+            ]
+        },
+        {
+            component: SearchResults,
+            if: state => state.isSearchView && state.hasQuery,
+            children: [
+                {
+                    component: SearchResult,
+                    forEach: state => state.results
+                }
+            ]
+        },
+        {
+            component: Movie,
+            if: state => state.isMovieView
+        },
+        Loader
+    ]
+};
