197ee03c79d541f7e339ac5195701cb63dc93638
Patrick Kunka <patrick@kunkalabs.com>
2017-08-04 22:29:22 +0100

    implement component ref querying and event binding/unbinding

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,6 +1,27 @@
 import {Component} from '../generic';
 
 class SearchForm extends Component {
+    constructor() {
+        super(...arguments);
+
+        this.configure({
+            refs: ['input'],
+            events: [
+                {
+                    ref: 'root',
+                    on: 'submit',
+                    bind: 'handleSubmit'
+                }
+            ]
+        });
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        console.log(this.refs.input.value);
+    }
+
     render(state, props, children) {
         return (
             `<form class="search-form">
