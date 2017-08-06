c03d6ab5ff7555eb24d62d4b060d228792adcf64
Patrick Kunka <patrick@kunkalabs.com>
2017-08-06 12:39:39 +0100

    Add year of release to suggestion text

--- src/components/Suggestion.js
+++ src/components/Suggestion.js
@@ -1,8 +1,9 @@
 import {Component} from '../generic';
+import Helpers from '../services/Helpers';
 
 class Suggestion extends Component {
     render(state, props) {
-        return `<a class="search-form__suggestion" href="/${props.id}/">${props.original_title}</a>`;
+        return `<a class="search-form__suggestion" href="/${props.id}/">${props.original_title}&nbsp;<span class="search-form__suggestion-year">${Helpers.year(props.release_date)}</span></a>`;
     }
 }
 
